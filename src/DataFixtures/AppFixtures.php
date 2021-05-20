b54035ee3b1ebf9f3414fe379063331fd26d97c9
AymerickD <aymerick.demay@hotmail.fr>
2021-05-20 14:39:56 +0200

    rebasage de l'espace de travail

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,23 +10,6 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $artwork = new Artwork();
-        // $manager->persist($artwork);
-
-        $manager->flush();
-        $faker = \Faker\Factory::create();
-        $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
-        $faker->addProvider(new \Liior\Faker\Prices($faker));
-
-        for ($i = 0; $i < 10; $i++) {
-            $artwork = new Artwork();
-            $artwork->setTitle($faker->artworkName)
-                ->setPrice($faker->price(20, 200))
-                ->setGallery($faker->imageUrl(400, 400));
-
-            $manager->persist($artwork);
-        }
-        $manager->flush();
 
     }
 }
