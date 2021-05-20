f1ab5ee17cff9ce9deabbd03a7775b95978e03b9
Ksoul59 <erim59@msn.com>
2021-05-20 14:05:18 +0200

    ajout au panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,9 +2,65 @@
 
 namespace App\Controller;
 
+use App\Repository\ArtworkRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Routing\Annotation\Route;
 
-class CartController extends AbstractController {
+class CartController extends AbstractController
+{
 
+    /**
+     * @Route ("/panier", name="cart")
+     */
+    public function index(SessionInterface $session, ArtworkRepository $artworkRepository)
+    {
+        $panier = $session->get('panier', []);
 
+        $panierWithdata = [];
+
+        foreach ($panier as $id => $id) {
+            $panierWithdata[] =
+                [
+                'artworks' => $artworkRepository->find($id),
+            ];
+        }
+
+        return $this->render("cart/index.html.twig", [
+            'items' => $panierWithdata,
+        ]);
+
+    }
+
+    /**
+     * @Route ("/panier/add/{id}", name="cart_add")
+     */
+    public function add($id, SessionInterface $session)
+    {
+
+        $panier = $session->get('panier', []);
+
+        $panier[$id] = 1;
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute("cart");
+
+    }
+
+    /**
+     * @Route("/panier/remove/{id}", name="cart_remove")
+     */
+    public function remove($id, SessionInterface $session)
+    {
+        $panier = $session->get('panier', []);
+
+        if (!empty($panier[$id])) {
+            unset($panier[$id]);
+        }
+
+        $session->set('panier', $panier);
+
+        return $this->redirectToRoute("cart");
+    }
 }
--- src/DataFixtures/TestFixtures .php
+++ src/DataFixtures/TestFixtures .php
@@ -4,29 +4,25 @@ namespace App\DataFixtures;
 
 use App\Entity\Artwork;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Persistence\ObjectManager;
+use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $artwork = new Artwork();
-        // $manager->persist($artwork);
-
-        $manager->flush();
         $faker = \Faker\Factory::create();
         $faker->addProvider(new \Bezhanov\Faker\Provider\Commerce($faker));
         $faker->addProvider(new \Liior\Faker\Prices($faker));
 
         for ($i = 0; $i < 10; $i++) {
-            $artwork = new Artwork();
-            $artwork->setTitle($faker->artworkName)
+            $product = new Product();
+            $product->setTitle($faker->productName)
                 ->setPrice($faker->price(20, 200))
-                ->setGallery($faker->imageUrl(400, 400));
+                ->setImage($faker->imageUrl(400, 400));
 
-            $manager->persist($artwork);
+            $manager->persist($product);
         }
-        $manager->flush();
 
+        $manager->flush();
     }
 }
